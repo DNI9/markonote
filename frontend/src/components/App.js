8fa6fb5e1bf60e1efbd1f79459a211841a68fef8
DNI9 <itsr9boi@gmail.com>
2020-10-05 21:48:25 +0530

    lazy load routes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,34 @@
-import {Box} from '@chakra-ui/core';
-import React from 'react';
+import {Box, Spinner} from '@chakra-ui/core';
+import React, {Suspense} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import '../../node_modules/highlight.js/styles/github.css';
 import '../Main.scss';
-import Home from './Pages/Home';
-import Note from './Pages/Note';
+
+const Home = React.lazy(() => import('./Pages/Home'));
+const Note = React.lazy(() => import('./Pages/Note'));
 
 export default function App() {
+  const LazyRoute = ({component: Component, ...rest}) => (
+    <Route
+      {...rest}
+      render={props => (
+        <Suspense
+          fallback={
+            <Box className='abs-center'>
+              <Spinner size='xl' />
+            </Box>
+          }>
+          <Component {...props} />
+        </Suspense>
+      )}
+    />
+  );
   return (
     <Router>
       <Box minH='100vh'>
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/:id' component={Note} />
+          <LazyRoute exact path='/' component={Home} />
+          <LazyRoute exact path='/:id' component={Note} />
         </Switch>
       </Box>
     </Router>
