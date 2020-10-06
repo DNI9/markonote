30997643cdb368e438f6b6a85893c049c0e7fce9
DNI9 <itsr9boi@gmail.com>
2020-10-06 10:42:13 +0530

    serve static files for production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
@@ -17,9 +18,19 @@ app.use(express.json());
 // DEFINE ROUTES
 app.use('/api/notes', noteRoutes);
 
-app.get('/', (req, res) => {
-  res.send('HELLO FROM BACKEND!');
-});
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('GUUNOBINDO API');
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running  on port ${PORT}`.yellow.bold));
