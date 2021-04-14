bcc4e1396e9fbb84995d80327b7fe5f6267a5c2c
DNI9 <itsr9boi@gmail.com>
2021-04-14 23:49:15 +0530

    add mongo local url

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,14 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI || 'mongodb://localhost',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.error(
