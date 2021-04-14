4b4de1fbdaf621302ed6aa1bba59211c8ebd672d
DNI9 <itsr9boi@gmail.com>
2021-04-14 23:50:24 +0530

    fix clipboard function

--- frontend/src/utils/CopyToClipboard.js
+++ frontend/src/utils/CopyToClipboard.js
@@ -1,18 +1,15 @@
 // TODO: change site name
 const SITE_NAME = 'http://localhost:3000';
-const CopyToClipboard = newClip => {
-  navigator.permissions.query({name: 'clipboard-write'}).then(result => {
-    if (result.state === 'granted' || result.state === 'prompt') {
-      navigator.clipboard.writeText(`${SITE_NAME}/${newClip}`).then(
-        function () {
-          console.log('COPIED');
-        },
-        function () {
-          console.error('COPY FAILED');
-        }
-      );
-    }
-  });
+const CopyToClipboard = async newClip => {
+  try {
+    await navigator.clipboard.writeText(`${SITE_NAME}/${newClip}`);
+    console.log('copied');
+  } catch (error) {
+    const {state} = await navigator.permissions.query({
+      name: 'clipboard-write',
+    });
+    console.log(error.message);
+  }
 };
 
 export default CopyToClipboard;
