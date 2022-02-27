d2a2366a0c5080a48bd0c8454de0d3f5f118d278
DNI9 <itsr9boi@gmail.com>
2022-02-27 20:10:12 +0530

    use window location for site url

--- frontend/src/utils/CopyToClipboard.js
+++ frontend/src/utils/CopyToClipboard.js
@@ -1,14 +1,10 @@
-// TODO: change site name
-const SITE_NAME = 'http://localhost:3000';
 const CopyToClipboard = async newClip => {
   try {
-    await navigator.clipboard.writeText(`${SITE_NAME}/${newClip}`);
+    await navigator.clipboard.writeText(`${window.location.href}${newClip}`);
     console.log('copied');
   } catch (error) {
-    const {state} = await navigator.permissions.query({
-      name: 'clipboard-write',
-    });
-    console.log(error.message);
+    await navigator.permissions.query({name: 'clipboard-write'});
+    console.error(error.message);
   }
 };
 
